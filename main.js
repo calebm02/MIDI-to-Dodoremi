8dacd798f556b1c800a79f30c6841774039f1051
calebm02 <caleb.micklus@gmail.com>
2023-10-22 20:45:56 -0400

    actually makes a json file now

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ let fs = require('fs');
 let noteLength = 0;
 //the entire length of the song
 let songLength = 0;
-var noteCCNumber;
-
+//the note number in MIDI CC
+var masterJson = [];
+var inputs = [];
 // BPM -> MS conversion
 const bpm = 120
 const framesPerBeat = 96;
@@ -16,29 +17,47 @@ const msPerFrames = 1000 / (framesPerBeat * beatsPerSecond);
 //ms per bar
 const barLength = msPerFrames * framesPerBeat * 4
 
-
+function addNotes (start, duration, noteCCNumber){
+    var notes
+    var notePosition = [
+        {start: 0, lanes:[5], notes: [start, duration, noteCCNumber]},
+    ];
+    inputs = inputs.concat(notePosition);
+}
 //reads the midi file and parses it at base64
 fs.readFile('./midi.mid', 'base64', function (err,data){
     var midiArray = midiParser.parse(data);
-    console.log(midiArray.track[0].event[1])
+    var noteCCNumber;
+    //console.log(midiArray)
     //for each event on the first track, find a noteOn message (type 9) and print "noteOn"
     midiArray.track[0].event.forEach(function(element){
         songLength += (element.deltaTime * msPerFrames);
-        //if the event is a noteOn get the CC data
+        //if the event is a noteOn get the CC data and push the last note
         if(element.type == 9){
+            addNotes(0, noteLength, noteCCNumber);
             //this outputs the midi CC note
             noteCCNumber = element.data[0];
             //resets the note length when the next note starts
             noteLength = 0;
         }else if (element.type == 8) {
             noteLength += (element.deltaTime * msPerFrames);
-            //console.log(noteLength);
         }
     });
     //length of song in bars (not fully tested yet)
     var songBarLength = (songLength / barLength);
-    //console.log(songBarLength);
+    writeJsonFile(inputs);
 }
 )
+
+function writeJsonFile(inputs){
+    masterJson.push(inputs);
+    const data = JSON.stringify(masterJson, null, 2);
+    fs.writeFile("config.json", data, (error) => {
+        if (error) {
+            console.error(error);
+            throw error;
+        }
+    });
+}
 
 
